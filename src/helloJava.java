830079f4c4d00b92e1c36c2c7bbdafc02e66d529
huang1630412 <1139552121>
2017-02-21 09:36:06 +0800

    dfdtf

---
+++ src/helloJava.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/2/21.
+ */
+public class helloJava {
+    public static void main(String[]args){
+        System.out.println("Thi is a New Java project!");
+    }
+}
